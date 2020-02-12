c285ffd070a8c9b71f33e2abf0df3db4e88f52db
sruedebusch <54645645+sruedebusch@users.noreply.github.com>
2020-02-11 23:33:05 -0600

    Update stringMethods.js

--- forms/stringMethods/stringMethods.js
+++ forms/stringMethods/stringMethods.js
@@ -20,6 +20,24 @@ console.log(lowerCaseString)
 let stringIndex = lowerCaseString[3]
 console.log(`The character at location 3 is ${stringIndex}`)
 
+let findString = secondQuote.indexOf('-') + 1
+let foundString = secondQuote.slice(findString)
+console.log(foundString)
+
+
+
+
+
+
+
+
+//let findString = secondQuote.slice(0, 120)
+//console.log(findString)
+
+
+
+
+
 
 
 
