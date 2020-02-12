ec1a9cb8a3ac4d94abdced32408c370e4d2c930d
sruedebusch <54645645+sruedebusch@users.noreply.github.com>
2020-02-11 21:28:47 -0600

    Update stringMethods.js

--- forms/stringMethods/stringMethods.js
+++ forms/stringMethods/stringMethods.js
@@ -1,18 +1,26 @@
 // This line of code declares the variable with the string and prints it
-let quoteString = ["Failure is simply the opportunity to begin again this time more intelligently"]
-alert(quoteString)
+let quoteString = "Failure is simply the opportunity to begin again this time more intelligently"
+console.log(quoteString)
 
-// This line of code changes the quoteString variable string to uppercase and prints it
-let upperCaseString = quoteString.toUpperCase();
-alert(`Upper case string is: ${upperCaseString}`);
+let upperCaseString = (quoteString.toUpperCase())
+console.log(`Upper case string is: ${upperCaseString}`)
 
-// This line of code creates the string, "-Henry Ford".
 let authorString = "-Henry Ford"
-alert(authorString)
+console.log(authorString)
 
-// This code puts authorString at the end of quoteString in order to create the completeString
-let completeString = authorString.push(quoteString)
-alert(completeString)
+let completeString = quoteString + " " + authorString
+console.log(completeString)
+
+let secondQuote = "Talent is cheaper than table salt. What separates the talented individual from the successful one is a lot of hard work. - Stephen King"
+console.log(secondQuote)
+
+let lowerCaseString = secondQuote.toLowerCase()
+console.log(lowerCaseString)
+
+let stringIndex = lowerCaseString[3]
+console.log(`The character at location 3 is ${stringIndex}`)
+
+
 
 
 
